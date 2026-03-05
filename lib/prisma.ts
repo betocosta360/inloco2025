43add2275226968325cf0f9028b0a9865fb1f5ed
robero costa de cerqueira <42754685+betocosta360@users.noreply.github.com>
2026-03-05 08:38:30 -0300

    alteração no prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,26 +1,15 @@
-import 'dotenv/config';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as {
-    prisma: PrismaClient | undefined,
-    pool: Pool | undefined
+  prisma: PrismaClient | undefined;
 };
 
-if (!globalForPrisma.pool) {
-    globalForPrisma.pool = new Pool({
-        connectionString: process.env.DATABASE_URL
-    });
-}
-
-const adapter = new PrismaPg(globalForPrisma.pool);
-
 export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        adapter,
-        log: ['error', 'warn']
-    });
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error', 'warn'],
+  });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
